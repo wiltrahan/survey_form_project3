fa59a85dd945fd060c9e259736fe93538fe32aaa
wiltrahan <twilorip@gmail.com>
2017-06-28 17:30:44 -0400

    add font, other styling

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
   $("#myForm").submit(function() {
-    alert("Thank you for your submission!");
+    alert("Thank you for your submission. Our marketing staff will be calling soon!");
   });
 });
